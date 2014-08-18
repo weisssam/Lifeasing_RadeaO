ba86778c0ba632590e2bcc24a8877f34b1a3bd45
weisssam <weiss.shmuel@gmail.com>
2014-08-18 22:47:40 +0300

    new project

--- Test1/gen/com/example/test1/R.java
+++ Test1/gen/com/example/test1/R.java
@@ -24,10 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int analogClock1=0x7f080005;
+        public static final int calendarView1=0x7f080007;
+        public static final int chronometer1=0x7f080006;
+        public static final int editText1=0x7f080000;
+        public static final int editText2=0x7f080001;
+        public static final int editText3=0x7f080002;
+        public static final int gallery1=0x7f080004;
+        public static final int imageView1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int linearlayout=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
