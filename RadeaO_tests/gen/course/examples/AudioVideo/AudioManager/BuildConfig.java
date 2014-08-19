651111be72c70f0654f30f7922746426e28238b5
weisssam <Shmuel@weiss-laptop-1>
2014-08-19 11:16:31 +0300

    Add new project for Android TESTS

---
+++ RadeaO_tests/gen/course/examples/AudioVideo/AudioManager/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package course.examples.AudioVideo.AudioManager;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
